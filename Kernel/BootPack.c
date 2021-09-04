23818f5d2127ef68aed60cac46a0438c49d14243
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-09-04 20:00:48 +0900

    ビデオメモリに書き込み

--- Kernel/BootPack.c
+++ Kernel/BootPack.c
@@ -1,10 +1,15 @@
 
 void io_hlt(void);
+void write_mem8(int addr, int data);
 
 void HariMain(void)
 {
+    int i;
+    for (i = 0xa0000; i <= 0xaffff; i++) {
+        write_mem8(i, 33);
+    }
 
-fin:
-    io_hlt();
-    goto fin;
+    for (;;) {
+        io_hlt();
+    }
 }
--- Kernel/Func.s
+++ Kernel/Func.s
@@ -1,9 +1,16 @@
 
 .code32
 .global     io_hlt
+.global     write_mem8
 
 .text
 
-io_hlt:
+io_hlt:         # void io_hlt(void)
     HLT
+    RET
+
+write_mem8:     # void write_mem8(int addr, int data)
+    MOVL    4(%ESP),    %ECX
+    MOVB    8(%ESP),    %AL
+    MOVB    %AL,    (%ECX)
     RET
