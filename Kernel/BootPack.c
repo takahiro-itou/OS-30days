a0cc58e4822f23e255010e5ac45b240fbea0594b
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-09-13 01:52:41 +0900

    文字列を表示する機能を実装

--- Kernel/BootPack.c
+++ Kernel/BootPack.c
@@ -11,6 +11,8 @@ void boxfill8(unsigned char *vram, int xsize, unsigned char c,
               int x0, int y0, int x1, int y1);
 void init_screen(char *vram, int x, int y);
 void putfont8(char *vram, int xsize, int x, int y, char c, const char *font);
+void putfonts8_asc(char *vram, int xsize, int x, int y,
+                   char c, const unsigned char *s);
 
 #define COL8_000000     0
 #define COL8_FF0000     1
@@ -43,19 +45,18 @@ void HariMain(void)
         0x00, 0x18, 0x18, 0x18,  0x18, 0x24, 0x24, 0x24,
         0x24, 0x7e, 0x42, 0x42,  0x42, 0xe7, 0x00, 0x00
     };
-    extern const char hankaku[4096];
 
     init_palette();
 
     binfo = (struct BOOTINFO *)(0xff0);
     init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
 
-    putfont8(binfo->vram, binfo->scrnx,  8, 8, COL8_FFFFFF, hankaku + 'A'*16);
-    putfont8(binfo->vram, binfo->scrnx, 16, 8, COL8_FFFFFF, hankaku + 'B'*16);
-    putfont8(binfo->vram, binfo->scrnx, 24, 8, COL8_FFFFFF, hankaku + 'C'*16);
-    putfont8(binfo->vram, binfo->scrnx, 40, 8, COL8_FFFFFF, hankaku + '1'*16);
-    putfont8(binfo->vram, binfo->scrnx, 48, 8, COL8_FFFFFF, hankaku + '2'*16);
-    putfont8(binfo->vram, binfo->scrnx, 56, 8, COL8_FFFFFF, hankaku + '3'*16);
+    putfonts8_asc(binfo->vram, binfo->scrnx,  8,  8,
+                  COL8_FFFFFF, "ABC 123");
+    putfonts8_asc(binfo->vram, binfo->scrnx, 31, 31,
+                  COL8_000000, "Haribote OS.");
+    putfonts8_asc(binfo->vram, binfo->scrnx, 30, 30,
+                  COL8_FFFFFF, "Haribote OS.");
 
     for (;;) {
         io_hlt();
@@ -149,6 +150,17 @@ void putfont8(char *vram, int xsize, int x, int y, char c, const char *font)
         if ((d & 0x04) != 0) { p[5] = c; }
         if ((d & 0x02) != 0) { p[6] = c; }
         if ((d & 0x01) != 0) { p[7] = c; }
+    }
+    return;
+}
+
+void putfonts8_asc(char *vram, int xsize, int x, int y,
+                   char c, const unsigned char *s)
+{
+    extern const char hankaku[4096];
+    for (; *s != 0x00; ++s) {
+        putfont8(vram, xsize, x, y, c, hankaku + (*s) * 16);
+        x += 8;
     }
     return;
 }
