e634177e8de83e75af61f265fd69b840ac191def
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-09-19 19:03:13 +0900

    マウスカーソルを描画

--- Kernel/BootPack.c
+++ Kernel/BootPack.c
@@ -16,6 +16,10 @@ void putfont8(char *vram, int xsize, int x, int y, char c, const char *font);
 void putfonts8_asc(char *vram, int xsize, int x, int y,
                    char c, const unsigned char *s);
 
+void init_mouse_cursor8(char *mouse, char bc);
+void putblock8_8(char *vram, int vxsize, int pxsize, int pysize,
+                 int px0, int py0, char *buf, int bxsize);
+
 #define COL8_000000     0
 #define COL8_FF0000     1
 #define COL8_00FF00     2
@@ -43,25 +47,18 @@ struct BOOTINFO
 void HariMain(void)
 {
     struct BOOTINFO *binfo = (struct BOOTINFO *)(0xff0);
-    char s[40];
-
-    static char font_A[16] = {
-        0x00, 0x18, 0x18, 0x18,  0x18, 0x24, 0x24, 0x24,
-        0x24, 0x7e, 0x42, 0x42,  0x42, 0xe7, 0x00, 0x00
-    };
-
+    char s[40], mcursor[256];
+    int mx, my;
     init_palette();
     init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
 
-    putfonts8_asc(binfo->vram, binfo->scrnx,  8,  8,
-                  COL8_FFFFFF, "ABC 123");
-    putfonts8_asc(binfo->vram, binfo->scrnx, 31, 31,
-                  COL8_000000, "Haribote OS.");
-    putfonts8_asc(binfo->vram, binfo->scrnx, 30, 30,
-                  COL8_FFFFFF, "Haribote OS.");
+    mx = (binfo->scrnx - 16) / 2;
+    my = (binfo->scrny - 28 - 16) / 2;
+    init_mouse_cursor8(mcursor, COL8_008484);
+    putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
 
-    snprintf(s, sizeof(s) - 1, "scrnx = %d", binfo->scrnx);
-    putfonts8_asc(binfo->vram, binfo->scrnx, 16, 64, COL8_FFFFFF, s);
+    snprintf(s, sizeof(s) - 1, "(%d, %d)", mx, my);
+    putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
     for (;;) {
         io_hlt();
@@ -166,6 +163,57 @@ void putfonts8_asc(char *vram, int xsize, int x, int y,
     for (; *s != 0x00; ++s) {
         putfont8(vram, xsize, x, y, c, hankaku + (*s) * 16);
         x += 8;
+    }
+    return;
+}
+
+/*  マウスカーソルを準備 (16x16).   */
+void init_mouse_cursor8(char *mouse, char bc)
+{
+    static const char cursor[16][16] = {
+        "**************..",
+        "*OOOOOOOOOOO*...",
+        "*OOOOOOOOOO*....",
+        "*OOOOOOOOO*.....",
+        "*OOOOOOOO*......",
+        "*OOOOOOO*.......",
+        "*OOOOOOO*.......",
+        "*OOOOOOOO*......",
+        "*OOOO**OOO*.....",
+        "*OOO*..*OOO*....",
+        "*OO*....*OOO*...",
+        "*O*......*OOO*..",
+        "**........*OOO*.",
+        "*..........*OOO*",
+        "............*OO*",
+        ".............***"
+    };
+    int x, y;
+
+    for (y = 0; y < 16; ++ y) {
+        for (x = 0; x < 16; ++ x) {
+            if (cursor[y][x] == '*') {
+                mouse[y * 16 + x] = COL8_000000;
+            }
+            if (cursor[y][x] == 'O') {
+                mouse[y * 16 + x] = COL8_FFFFFF;
+            }
+            if (cursor[y][x] == '.') {
+                mouse[y * 16 + x] = bc;
+            }
+        }
+    }
+    return;
+}
+
+void putblock8_8(char *vram, int vxsize, int pxsize, int pysize,
+                 int px0, int py0, char *buf, int bxsize)
+{
+    int x, y;
+    for (y = 0; y < pysize; ++ y) {
+        for (x = 0; x < pxsize; ++ x) {
+            vram[(py0 + y) * vxsize + (px0 + x)] = buf[y * bxsize + x];
+        }
     }
     return;
 }
