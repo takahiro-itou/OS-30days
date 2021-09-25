17602d6e282dd630df22e37e5b706ed8fa22c218
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-09-25 22:08:34 +0900

    マウスの解読

--- Kernel/BootPack.c
+++ Kernel/BootPack.c
@@ -8,6 +8,7 @@ void HariMain(void)
     struct BOOTINFO *binfo = (struct BOOTINFO *)(0xff0);
     char s[40], mcursor[256], keybuf[32], mousebuf[128];
     int mx, my, i;
+    unsigned char mouse_dbuf[3], mouse_phase;
 
     init_gdtidt();
     init_pic();
@@ -32,6 +33,7 @@ void HariMain(void)
     putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
     enable_mouse();
+    mouse_phase = 0;    /*  マウスの 0xfa を待っている段階へ。  */
 
     for (;;) {
         io_cli();
@@ -46,9 +48,32 @@ void HariMain(void)
         } else if (fifo8_status(&mousefifo) != 0) {
             i = fifo8_get(&mousefifo);
             io_sti();
-            snprintf(s, sizeof(s), "%02x", i);
-            boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 47, 31);
-            putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+            if (mouse_phase == 0 ) {
+                /*  マウスの 0xfa を待っている段階  */
+                if (i == 0xfa) {
+                    mouse_phase = 1;
+                }
+            } else if (mouse_phase == 1) {
+                /*  マウスの１バイト目を待っている段階  */
+                mouse_dbuf[0] = i;
+                mouse_phase = 2;
+            } else if (mouse_phase == 2) {
+                /*  マウスの２バイト目を待っている段階  */
+                mouse_dbuf[1] = i;
+                mouse_phase = 3;
+            } else {
+                /*  マウスの３バイト目を待っている段階  */
+                mouse_dbuf[2] = i;
+                mouse_phase = 1;
+                /*  データが揃ったので表示  */
+                snprintf(
+                        s, sizeof(s), "%02x %02x %02x",
+                        mouse_dbuf[0], mouse_dbuf[1], mouse_dbuf[2]);
+                boxfill8(binfo->vram, binfo->scrnx, COL8_008484,
+                         32, 16, 32 + 8 * 8 - 1, 31);
+                putfonts8_asc(binfo->vram, binfo->scrnx,
+                              32, 16, COL8_FFFFFF, s);
+            }
         }
     }
 }
