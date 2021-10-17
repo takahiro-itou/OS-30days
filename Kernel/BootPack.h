1e6d888da742fabef3f751b42180a1c1608a554f
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-10-17 20:25:33 +0900

    重ね合わせ処理を実装

--- Kernel/BootPack.h
+++ Kernel/BootPack.h
@@ -74,7 +74,7 @@ void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
 void boxfill8(unsigned char *vram, int xsize, unsigned char c,
               int x0, int y0, int x1, int y1);
-void init_screen(char *vram, int x, int y);
+void init_screen8(char *vram, int x, int y);
 void putfont8(char *vram, int xsize, int x, int y, char c, const char *font);
 void putfonts8_asc(char *vram, int xsize, int x, int y,
                    char c, const unsigned char *s);
@@ -200,3 +200,30 @@ void inthandler2c(int *esp);
 
 void enable_mouse(struct MOUSE_DEC *mdec);
 int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
+
+
+/*  Sheet.c     */
+
+#define MAX_SHEETS  256
+
+struct SHEET {
+    unsigned char *buf;
+    int bxsize, bysize, vx0, vy0, col_inv, height, flags;
+};
+
+struct SHTCTL {
+    unsigned char *vram;
+    int xsize, ysize, top;
+    struct SHEET *sheets[MAX_SHEETS];
+    struct SHEET sheets0[MAX_SHEETS];
+};
+
+struct SHTCTL *shtctl_init(struct MEMMAN *memman, unsigned char *vram,
+                           int xsize, int ysize);
+struct SHEET *sheet_alloc(struct SHTCTL *ctl);
+void sheet_setbuf(struct SHEET *sht, unsigned char *buf,
+                  int xsize, int ysize, int col_inv);
+void sheet_updown(struct SHTCTL *ctl, struct SHEET *sht, int height);
+void sheet_refresh(struct SHTCTL *ctl);
+void sheet_slide(struct SHTCTL *ctl, struct SHEET *sht, int vx0, int vy0);
+void sheet_free(struct SHTCTL *ctl, struct SHEET *sht);
