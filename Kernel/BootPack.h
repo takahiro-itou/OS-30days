8888baa470178fb58576bc4fb89482d241472aa4
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-10-17 19:34:53 +0900

    メモリ管理モジュールを変更

--- Kernel/BootPack.h
+++ Kernel/BootPack.h
@@ -185,6 +185,9 @@ unsigned int memman_total(struct MEMMAN *man);
 unsigned int memman_alloc(struct MEMMAN *man, unsigned int size);
 int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
 
+unsigned int memman_alloc_4k(struct MEMMAN *man, unsigned int size);
+int memman_free_4k(struct MEMMAN *man, unsigned int addr, unsigned int size);
+
 
 /*  Mouse.c     */
 
