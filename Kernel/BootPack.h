e4ea0c4a3553bdc6380648d96f9997407f09f12e
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-10-24 14:10:40 +0900

    複数タイマを実装

--- Kernel/BootPack.h
+++ Kernel/BootPack.h
@@ -234,15 +234,29 @@ void sheet_free(struct SHEET *sht);
 
 /*  Timer.c     */
 
-struct TIMERCTL {
-    unsigned int count;
-    unsigned int timeout;
+#define MAX_TIMER   500
+
+#define TIMER_FLAGS_ALLOC   1   /*  確保した状態。  */
+#define TIMER_FLAGS_USING   2   /*  タイマ作動中。  */
+
+struct TIMER {
+    unsigned int timeout, flags;
     struct FIFO8 * fifo;
     unsigned char data;
+};
+
+struct TIMERCTL {
+    unsigned int count;
+    struct TIMER timer[MAX_TIMER];
 };
 
 extern struct TIMERCTL timerctl;
 
 void init_pit(void);
+struct TIMER *timer_alloc(void);
+void timer_free(struct TIMER *timer);
+void timer_init(struct TIMER *timer, struct FIFO8 *fifo, unsigned char data);
+void timer_settime(struct TIMER *timer, unsigned int timeout);
+
 void inthandler20(int *esp);
-void settimer(unsigned int timeout, struct FIFO8 *fifo, unsigned char data);
+
