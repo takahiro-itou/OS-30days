4354845ddcdbfb321ddacedeb543a69607bc212c
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-10-26 23:30:01 +0900

    マウスクリックでウィンドウを移動

--- Kernel/BootPack.c
+++ Kernel/BootPack.c
@@ -167,6 +167,10 @@ void HariMain(void)
                     putfonts8_asc_sht(sht_back, 0, 0, COL8_FFFFFF,
                                       COL8_008484, s, 10);
                     sheet_slide(sht_mouse, mx, my);
+                    if ((mdec.btn & 0x01) != 0) {
+                        /*  左ボタンを押していたら、sht_win を動かす。  */
+                        sheet_slide(sht_win, mx - 80, my - 8);
+                    }
                 }
             } else if (i == 10) {
                 putfonts8_asc_sht(sht_back, 0, 64, COL8_FFFFFF,
