25eedb6481c579235573a92800387cb3d8be8fdb
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-11-01 23:16:18 +0900

    リファクタリング

--- Kernel/BootPack.c
+++ Kernel/BootPack.c
@@ -208,8 +208,7 @@ void HariMain(void)
     }
 }
 
-void make_window8(unsigned char *buf, int xsize, int ysize,
-                  char *title, char act)
+void make_wtitle8(unsigned char *buf, int xsize, char *title, char act)
 {
     static const char closebtn[14][16] = {
         "OOOOOOOOOOOOOOO@",
@@ -238,17 +237,7 @@ void make_window8(unsigned char *buf, int xsize, int ysize,
         tc  = COL8_C6C6C6;
         tbc = COL8_848484;
     }
-
-    boxfill8(buf, xsize, COL8_C6C6C6, 0, 0,         xsize - 1, 0        );
-    boxfill8(buf, xsize, COL8_FFFFFF, 1, 1,         xsize - 2, 1        );
-    boxfill8(buf, xsize, COL8_C6C6C6, 0, 0,         0,         ysize - 1);
-    boxfill8(buf, xsize, COL8_FFFFFF, 1, 1,         1,         ysize - 2);
-    boxfill8(buf, xsize, COL8_848484, xsize - 2, 1, xsize - 2, ysize - 2);
-    boxfill8(buf, xsize, COL8_000000, xsize - 1, 0, xsize - 1, ysize - 1);
-    boxfill8(buf, xsize, COL8_C6C6C6, 2, 2,         xsize - 3, ysize - 3);
-    boxfill8(buf, xsize, tbc,         3, 3,         xsize - 4, 20       );
-    boxfill8(buf, xsize, COL8_848484, 1, ysize - 2, xsize - 2, ysize - 2);
-    boxfill8(buf, xsize, COL8_000000, 0, ysize - 1, xsize - 1, ysize - 1);
+    boxfill8(buf, xsize, tbc, 3, 3, xsize - 4, 20);
     putfonts8_asc(buf, xsize, 24, 4, tc, title);
 
     for (y = 0; y < 14; ++ y) {
@@ -266,6 +255,22 @@ void make_window8(unsigned char *buf, int xsize, int ysize,
             buf[(5 + y) * xsize + (xsize - 21 + x)] = c;
         }
     }
+    return;
+}
+
+void make_window8(unsigned char *buf, int xsize, int ysize,
+                  char *title, char act)
+{
+
+    boxfill8(buf, xsize, COL8_C6C6C6, 0, 0,         xsize - 1, 0        );
+    boxfill8(buf, xsize, COL8_FFFFFF, 1, 1,         xsize - 2, 1        );
+    boxfill8(buf, xsize, COL8_C6C6C6, 0, 0,         0,         ysize - 1);
+    boxfill8(buf, xsize, COL8_FFFFFF, 1, 1,         1,         ysize - 2);
+    boxfill8(buf, xsize, COL8_848484, xsize - 2, 1, xsize - 2, ysize - 2);
+    boxfill8(buf, xsize, COL8_000000, xsize - 1, 0, xsize - 1, ysize - 1);
+    boxfill8(buf, xsize, COL8_C6C6C6, 2, 2,         xsize - 3, ysize - 3);
+    boxfill8(buf, xsize, COL8_848484, 1, ysize - 2, xsize - 2, ysize - 2);
+    boxfill8(buf, xsize, COL8_000000, 0, ysize - 1, xsize - 1, ysize - 1);
     return;
 }
 
