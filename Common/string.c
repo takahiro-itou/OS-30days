b6c406c437eac8b5aa3b93b04bf08ebff691b831
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-11-06 07:52:25 +0900

    標準ライブラリ strncmp  を実装

--- Common/string.c
+++ Common/string.c
@@ -12,3 +12,15 @@ int strcmp(const char *s1, const char *s2)
     }
     return ((unsigned char)*s1 - (unsigned char)*s2);
 }
+
+int strncmp(const char *s1, const char *s2, size_t n)
+{
+    while ( (-- n > 0) && (*s1 == *s2) ) {
+        if (*s1 == '\0') {
+            return 0;
+        }
+        ++ s1;
+        ++ s2;
+    }
+    return ((unsigned char)*s1 - (unsigned char)*s2);
+}
--- Common/string.h
+++ Common/string.h
@@ -2,6 +2,9 @@
 #if !defined( OS_INCLUDED_COMMON_STRING_H )
 #    define   OS_INCLUDED_COMMON_STRING_H
 
+typedef unsigned int    size_t;
+
 int strcmp(const char *s1, const char *s2);
+int strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
