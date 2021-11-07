e0c3c6e8f7a7e46731b1c07e850195f089070034
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-11-07 21:16:19 +0900

    リファクタリング

--- Kernel/BootPack.c
+++ Kernel/BootPack.c
@@ -2,11 +2,9 @@
 
 #include "BootPack.h"
 #include "../Common/stdio.h"
-#include "../Common/string.h"
 
 #define KEYCMD_LED      0xed
 
-void console_task(struct SHEET *sheet, unsigned int memtotal);
 
 void HariMain(void)
 {
@@ -339,35 +337,4 @@ void HariMain(void)
             }
         }
     }
-}
-
-void file_readfat(int *fat, unsigned char *img)
-{
-    int i, j = 0;
-    for (i = 0; i < 2880; i += 2) {
-        fat[i + 0] = (img[j + 0]      | img[j + 1] << 8) & 0xfff;
-        fat[i + 1] = (img[j + 1] >> 4 | img[j + 2] << 4) & 0xfff;
-        j += 3;
-    }
-    return;
-}
-
-void file_loadfile(int clustno, int size, char *buf, int *fat, char *img)
-{
-    int i;
-    for (;;) {
-        if (size <= 512) {
-            for (i = 0; i < size; ++ i) {
-                buf[i] = img[clustno * 512 + i];
-            }
-            break;
-        }
-        for (i = 0; i < 512; ++ i) {
-            buf[i] = img[clustno * 512 + i];
-        }
-        size -= 512;
-        buf  += 512;
-        clustno = fat[clustno];
-    }
-    return;
 }
