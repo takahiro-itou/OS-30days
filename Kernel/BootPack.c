b3ef0d458285e35062ce5c939715b9f8f805c5c4
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-11-07 20:36:55 +0900

    type  コマンド改良

--- Kernel/BootPack.c
+++ Kernel/BootPack.c
@@ -612,13 +612,37 @@ type_next_file:
                             for (x = 0; x < y; ++ x) {
                                 s[0] = p[x];
                                 s[1] = 0;
-                                putfonts8_asc_sht(sheet, cursor_x, cursor_y,
-                                                  COL8_FFFFFF, COL8_000000,
-                                                  s, 1);
-                                cursor_x += 8;
-                                if (cursor_x == 8 + 240) {
+                                if (s[0] == 0x09) {
+                                    for(;;) {
+                                        putfonts8_asc_sht(sheet, cursor_x,
+                                                          cursor_y,
+                                                          COL8_FFFFFF,
+                                                          COL8_000000,
+                                                          " ", 1);
+                                        cursor_x += 8;
+                                        if (cursor_x == 8 + 240) {
+                                            cursor_x = 8;
+                                            cursor_y = cons_newline(cursor_y,
+                                                                    sheet);
+                                        }
+                                        if (((cursor_x - 8) & 0x1f) == 0) {
+                                            break;
+                                        }
+                                    }
+                                } else if (s[0]== 0x0a) {
                                     cursor_x = 8;
                                     cursor_y = cons_newline(cursor_y, sheet);
+                                } else if (s[0] == 0x0d) {
+                                } else {
+                                    putfonts8_asc_sht(sheet, cursor_x, cursor_y,
+                                                  COL8_FFFFFF, COL8_000000,
+                                                  s, 1);
+                                    cursor_x += 8;
+                                    if (cursor_x == 8 + 240) {
+                                        cursor_x = 8;
+                                        cursor_y = cons_newline(cursor_y,
+                                                                sheet);
+                                    }
                                 }
                             }
                         } else {
