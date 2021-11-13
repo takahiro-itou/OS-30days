628eb5e81d782f3a63f526e948e1d5ea97be0d9e
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-11-13 14:46:52 +0900

    リファクタリング

--- Kernel/Console.c
+++ Kernel/Console.c
@@ -8,13 +8,20 @@ void console_task(struct SHEET *sheet, unsigned int memtotal)
 {
     struct TIMER *timer;
     struct TASK *task = task_now();
-    int i, fifobuf[128], cursor_x = 16, cursor_y = 28, cursor_c = -1;
-    char s[30], cmdline[30], *p;
     struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
+    int i, fifobuf[128];
+    int *fat = (int *) memman_alloc_4k(memman, 4 * 2880);
+    struct CONSOLE cons;
+    //int cursor_x = 16, cursor_y = 28, cursor_c = -1;
+    char s[30], cmdline[30], *p;
     int x, y;
     struct FILEINFO *finfo = (struct FILEINFO *) (ADR_DISKIMG + 0x002600);
-    int *fat = (int *) memman_alloc_4k(memman, 4 * 2880);
     struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *) ADR_GDT;
+
+    cons.sht = sheet;
+    cons.cur_x =  8;
+    cons.cur_y = 28;
+    cons.cur_c = -1;
 
     fifo32_init(&task->fifo, sizeof(fifobuf), fifobuf, task);
 
@@ -37,60 +44,62 @@ void console_task(struct SHEET *sheet, unsigned int memtotal)
             if (i <= 1) {   /*  カーソル用タイマ。  */
                 if (i != 0) {
                     timer_init(timer, &task->fifo, 0);
-                    if (cursor_c >= 0) {
-                        cursor_c = COL8_FFFFFF;
+                    if (cons.cur_c >= 0) {
+                        cons.cur_c = COL8_FFFFFF;
                     }
                 } else {
                     timer_init(timer, &task->fifo, 1);
-                    if (cursor_c >= 0) {
-                        cursor_c = COL8_000000;
+                    if (cons.cur_c >= 0) {
+                        cons.cur_c = COL8_000000;
                     }
                 }
                 timer_settime(timer, 50);
             }
             if (i == 2) {       /*  カーソル ON */
-                cursor_c = COL8_FFFFFF;
+                cons.cur_c = COL8_FFFFFF;
             }
             if (i == 3) {       /*  カーソル OFF    */
                 boxfill8(sheet->buf, sheet->bxsize, COL8_000000,
-                         cursor_x, cursor_y, cursor_x + 7, cursor_y + 15);
-                cursor_c = -1;
+                         cons.cur_x, cons.cur_y,
+                         cons.cur_x + 7, cons.cur_y + 15);
+                cons.cur_c = -1;
             }
             if (256 <= i && i <= 511) {
                 /*  キーボードデータ。  */
                 if (i == 8 + 256) {
                     /*  バックスペース  */
-                    if (cursor_x > 16) {
-                        putfonts8_asc_sht(sheet, cursor_x, cursor_y,
+                    if (cons.cur_x > 16) {
+                        putfonts8_asc_sht(sheet, cons.cur_x, cons.cur_y,
                                           COL8_FFFFFF, COL8_000000, " ", 1);
-                        cursor_x -= 8;
+                        cons.cur_x -= 8;
                     }
                 } else if (i == 10 + 256) {
                     /*  Enter.  */
                     /*  カーソルをスペースで消す。  */
-                    putfonts8_asc_sht(sheet, cursor_x, cursor_y,
+                    putfonts8_asc_sht(sheet, cons.cur_x, cons.cur_y,
                                       COL8_FFFFFF, COL8_000000, " ", 1);
-                    cmdline[cursor_x / 8 - 2] = 0;
-                    cursor_y = cons_newline(cursor_y, sheet);
+                    cmdline[cons.cur_x / 8 - 2] = 0;
+                    cons_newline(&cons);
+
                     /*  コマンド実行。  */
                     if (strcmp(cmdline, "mem") == 0) {
                         /*  mem コマンド。  */
                         snprintf(s, sizeof(s), "total   %dMB",
                                  memtotal / (1024 * 1024));
-                        putfonts8_asc_sht(sheet, 8, cursor_y, COL8_FFFFFF,
+                        putfonts8_asc_sht(sheet, 8, cons.cur_y, COL8_FFFFFF,
                                           COL8_000000, s, 30);
-                        cursor_y = cons_newline(cursor_y, sheet);
+                        cons_newline(&cons);
                         snprintf(s, sizeof(s), "free %dKB",
                                  memman_total(memman) / 1024);
-                        putfonts8_asc_sht(sheet, 8, cursor_y, COL8_FFFFFF,
+                        putfonts8_asc_sht(sheet, 8, cons.cur_y, COL8_FFFFFF,
                                           COL8_000000, s, 30);
-                        cursor_y = cons_newline(cursor_y, sheet);
-                        cursor_y = cons_newline(cursor_y, sheet);
+                        cons_newline(&cons);
+                        cons_newline(&cons);
                     } else if (strcmp(cmdline, "cls") == 0) {
                         boxfill8(sheet->buf, sheet->bxsize, COL8_000000,
                                  8, 28, 8 + 240 - 1, 28 + 128 - 1);
                         sheet_refresh(sheet, 8, 28, 8 + 240, 28 + 128);
-                        cursor_y = 28;
+                        cons.cur_y = 28;
                     } else if (strcmp(cmdline, "dir") == 0) {
                         for (x = 0; x < 224; ++ x) {
                             if (finfo[x].name[0] == 0x00) {
@@ -108,14 +117,14 @@ void console_task(struct SHEET *sheet, unsigned int memtotal)
                                 s[ 9] = finfo[x].ext[0];
                                 s[10] = finfo[x].ext[1];
                                 s[11] = finfo[x].ext[2];
-                                putfonts8_asc_sht(sheet, 8, cursor_y,
+                                putfonts8_asc_sht(sheet, 8, cons.cur_y,
                                                   COL8_FFFFFF,
                                                   COL8_000000,
                                                   s, 30);
-                                cursor_y = cons_newline(cursor_y, sheet);
+                                cons_newline(&cons);
                             }
                         }
-                        cursor_y = cons_newline(cursor_y, sheet);
+                        cons_newline(&cons);
                     } else if (strncmp(cmdline, "type ", 5) == 0) {
                         /*  ファイル名を準備する。  */
                         for (y = 0; y < 11; ++ y) {
@@ -162,52 +171,51 @@ type_next_file:
                             file_loadfile(finfo[x].clustno, finfo[x].size,
                                           p, fat,
                                           (char *) (ADR_DISKIMG + 0x003e00));
-                            cursor_x = 8;
+                            cons.cur_x = 8;
                             for (y = 0; y < finfo[x].size; ++ y) {
                                 s[0] = p[y];
                                 s[1] = 0;
                                 if (s[0] == 0x09) {
                                     for(;;) {
-                                        putfonts8_asc_sht(sheet, cursor_x,
-                                                          cursor_y,
+                                        putfonts8_asc_sht(sheet, cons.cur_x,
+                                                          cons.cur_y,
                                                           COL8_FFFFFF,
                                                           COL8_000000,
                                                           " ", 1);
-                                        cursor_x += 8;
-                                        if (cursor_x == 8 + 240) {
-                                            cursor_x = 8;
-                                            cursor_y = cons_newline(cursor_y,
-                                                                    sheet);
+                                        cons.cur_x += 8;
+                                        if (cons.cur_x == 8 + 240) {
+                                            cons.cur_x = 8;
+                                            cons_newline(&cons);
                                         }
-                                        if (((cursor_x - 8) & 0x1f) == 0) {
+                                        if (((cons.cur_x - 8) & 0x1f) == 0) {
                                             break;
                                         }
                                     }
                                 } else if (s[0]== 0x0a) {
-                                    cursor_x = 8;
-                                    cursor_y = cons_newline(cursor_y, sheet);
+                                    cons.cur_x = 8;
+                                    cons_newline(&cons);
                                 } else if (s[0] == 0x0d) {
                                 } else {
-                                    putfonts8_asc_sht(sheet, cursor_x, cursor_y,
+                                    putfonts8_asc_sht(sheet,
+                                                  cons.cur_x, cons.cur_y,
                                                   COL8_FFFFFF, COL8_000000,
                                                   s, 1);
-                                    cursor_x += 8;
-                                    if (cursor_x == 8 + 240) {
-                                        cursor_x = 8;
-                                        cursor_y = cons_newline(cursor_y,
-                                                                sheet);
+                                    cons.cur_x += 8;
+                                    if (cons.cur_x == 8 + 240) {
+                                        cons.cur_x = 8;
+                                        cons_newline(&cons);
                                     }
                                 }
                             }
                             memman_free_4k(memman, (int)p, finfo[x].size);
                         } else {
                             /*  ファイルが見つからなかった場合  */
-                            putfonts8_asc_sht(sheet, 8, cursor_y,
+                            putfonts8_asc_sht(sheet, 8, cons.cur_y,
                                               COL8_FFFFFF, COL8_000000,
                                               "File not found.", 15);
-                            cursor_y = cons_newline(cursor_y, sheet);
+                            cons_newline(&cons);
                         }
-                        cursor_y = cons_newline(cursor_y, sheet);
+                        cons_newline(&cons);
                     } else if (strcmp(cmdline, "hlt") == 0) {
                         /*  hlt.hrb アプリケーションを起動  */
                         for (y = 0; y < 11; ++ y) {
@@ -251,54 +259,54 @@ hlt_next_file:
                             memman_free_4k(memman, (int) p, finfo[x].size);
                         } else {
                             /*  ファイルが見つからなかった場合  */
-                            putfonts8_asc_sht(sheet, 8, cursor_y,
+                            putfonts8_asc_sht(sheet, 8, cons.cur_y,
                                               COL8_FFFFFF, COL8_000000,
                                               "File not found.", 15);
-                            cursor_y = cons_newline(cursor_y, sheet);
+                            cons_newline(&cons);
                         }
-                        cursor_y = cons_newline(cursor_y, sheet);
+                        cons_newline(&cons);
                     } else if (cmdline[0] != 0) {
                         /*  コマンドではなく、さらに空行でもない。  */
-                        putfonts8_asc_sht(sheet, 8, cursor_y, COL8_FFFFFF,
+                        putfonts8_asc_sht(sheet, 8, cons.cur_y, COL8_FFFFFF,
                                           COL8_000000, "Bad command.", 12);
-                        cursor_y = cons_newline(cursor_y, sheet);
-                        cursor_y = cons_newline(cursor_y, sheet);
+                        cons_newline(&cons);
+                        cons_newline(&cons);
                     }
                     /*  プロンプト表示  */
-                    putfonts8_asc_sht(sheet, 8, cursor_y,
+                    putfonts8_asc_sht(sheet, 8, cons.cur_y,
                                       COL8_FFFFFF, COL8_000000, ">", 1);
-                    cursor_x = 16;
+                    cons.cur_x = 16;
                 } else {
-                    if (cursor_x < 240) {
+                    if (cons.cur_x < 240) {
                         s[0] = i - 256;
                         s[1] = 0;
-                        cmdline[cursor_x / 8 - 2] = i - 256;
-                        putfonts8_asc_sht(sheet, cursor_x, cursor_y,
+                        cmdline[cons.cur_x / 8 - 2] = i - 256;
+                        putfonts8_asc_sht(sheet, cons.cur_x, cons.cur_y,
                                           COL8_FFFFFF, COL8_000000, s, 1);
-                        cursor_x += 8;
+                        cons.cur_x += 8;
                     }
                 }
             }
             /*  カーソル再表示  */
-            boxfill8(sheet->buf, sheet->bxsize, cursor_c,
-                     cursor_x, cursor_y, cursor_x + 7, cursor_y + 15);
-            sheet_refresh(sheet, cursor_x, cursor_y,
-                          cursor_x + 8, cursor_y + 16);
+            boxfill8(sheet->buf, sheet->bxsize, cons.cur_c,
+                     cons.cur_x, cons.cur_y, cons.cur_x + 7, cons.cur_y + 15);
+            sheet_refresh(sheet, cons.cur_x, cons.cur_y,
+                          cons.cur_x + 8, cons.cur_y + 16);
         }
     }
 }
 
-int cons_newline(int cursor_y, struct SHEET *sheet)
+void cons_newline(struct CONSOLE *cons)
 {
     int x, y;
-
-    if (cursor_y < 28 + 112) {
-        cursor_y += 16;     /*  次の行へ。  */
+    struct SHEET *sheet = cons->sht;
+    if (cons->cur_y < 28 + 112) {
+        cons->cur_y += 16;      /*  次の行へ。  */
     } else {
         /*  スクロール  */
         for (y = 28; y < 28 + 112; ++ y) {
             for (x = 8; x < 8 + 240; ++ x){
-                sheet->buf[x + y*sheet->bxsize] =
+                sheet->buf[x + y * sheet->bxsize] =
                         sheet->buf[x + (y + 16) * sheet->bxsize];
             }
         }
@@ -306,6 +314,6 @@ int cons_newline(int cursor_y, struct SHEET *sheet)
                  8, 28 + 112, 8 + 240 - 1, 28 + 128 - 1);
         sheet_refresh(sheet, 8, 28, 8 + 240, 28 + 128);
     }
-
-    return cursor_y;
+    cons->cur_x = 8;
+    return;
 }
