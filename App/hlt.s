eff64d3e458eefac260e4cd4eaffbc1a6298ccb8
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-11-14 15:23:53 +0900

    アプリケーションの内容を変更

--- App/hlt.s
+++ App/hlt.s
@@ -1,6 +1,14 @@
 
 .code32
 
-    MOVB    $'A',   %AL
+    MOVB    $'h',   %AL
+    LCALLL  $2*8, $0x0bbd
+    MOVB    $'e',   %Al
+    LCALLL  $2*8, $0x0bbd
+    MOVB    $'l',   %Al
+    LCALLL  $2*8, $0x0bbd
+    MOVB    $'l',   %Al
+    LCALLL  $2*8, $0x0bbd
+    MOVB    $'o',   %Al
     LCALLL  $2*8, $0x0bbd
     LRET
