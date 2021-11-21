89d94b60259edc7cb6a0a3ddfc9f1fa3ce0dc404
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-11-21 11:02:19 +0900

    例外が発生するようにテスト用アプリを修正

--- App/bug1.c
+++ App/bug1.c
@@ -4,12 +4,14 @@ void api_end(void);
 
 void HariMain(void)
 {
-    char a[100];
+    volatile char a[100];
     a[10] = 'A';        /*  これはもちろんいい  */
     api_putchar(a[10]);
     a[102] = 'B';       /*  これはまずいよね。  */
     api_putchar(a[102]);
     a[123] = 'C';       /*  これもまずいよね。  */
     api_putchar(a[123]);
+    a[133] = 'D';
+    api_putchar(a[133]);
     api_end();
 }
