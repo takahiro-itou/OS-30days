d1635f842585b89f99ea9f746c5c73abd5fa9d94
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-11-21 13:57:17 +0900

    ウィンドウを出す API  を追加

--- App/a_as.s
+++ App/a_as.s
@@ -3,6 +3,7 @@
 .globl      api_putchar
 .globl      api_putstr0
 .globl      api_end
+.globl      api_openwin
 
 .text
 
@@ -23,3 +24,20 @@ api_putstr0:    # void api_putstr0(char *s)
 api_end:        # void api_end(void)
     MOVL    $4,     %EDX
     INT     $0x40
+
+api_openwin:    # int api_openwin(char *buf, int xsiz, int ysiz,
+                #                 int col_inv, char *title)
+    PUSHL   %EDI
+    PUSHL   %ESI
+    PUSHL   %EBX
+    MOVL    $5,     %EDX
+    MOVL    16(%ESP),   %EBX    # buf
+    MOVL    20(%ESP),   %ESI    # xsiz
+    MOVL    24(%ESP),   %EDI    # ysiz
+    MOVL    28(%ESP),   %EAX    # col_inv
+    MOVL    32(%ESP),   %ECX    # title
+    INT     $0x40
+    POPL    %EBX
+    POPL    %ESI
+    POPL    %EDI
+    RET
