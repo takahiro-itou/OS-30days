e25e929365dcd152c18b8dbeb92c9cb123394698
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-11-27 17:46:14 +0900

    API 呼び出しを実装

--- App/a_as.s
+++ App/a_as.s
@@ -11,6 +11,7 @@
 .globl      api_free
 .globl      api_point
 .globl      api_refreshwin
+.globl      api_linewin
 
 .text
 
@@ -145,6 +146,25 @@ api_refreshwin:
     MOVL    32(%ESP),   %EDI    # y2
     INT     $0x40
     POPL    %EBX
+    POPL    %ESI
+    POPL    %EDI
+    RET
+
+api_linewin:
+    PUSHL   %EDI
+    PUSHL   %ESI
+    PUSHL   %EBP
+    PUSHL   %EBX
+    MOVL    $13,    %EDX
+    MOVL    20(%ESP),   %EBX    # win
+    MOVL    24(%ESP),   %EAX    # x0
+    MOVL    28(%ESP),   %ECX    # y0
+    MOVL    32(%ESP),   %ESI    # x1
+    MOVL    36(%ESP),   %EDI    # y1
+    MOVL    40(%ESP),   %EBP    # col
+    INT     $0x40
+    POPL    %EBX
+    POPL    %EBP
     POPL    %ESI
     POPL    %EDI
     RET
