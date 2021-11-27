2b682cacbc090e8e6088d44a87cf6d7a6d431d3d
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-11-27 17:55:18 +0900

    ウィンドウを閉じる API  を追加

--- Kernel/Console.c
+++ Kernel/Console.c
@@ -408,6 +408,8 @@ int *hrb_api(int edi, int esi, int ebp, int esp,
         if ((ebx & 1) == 0) {
             sheet_refresh(sht, eax, ecx, esi + 1, edi + 1);
         }
+    } else if (edx == 14) {
+        sheet_free((struct SHEET *) ebx);
     }
 
     return 0;
