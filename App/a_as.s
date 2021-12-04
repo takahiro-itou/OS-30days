94ecf9a305ac16d02738967ac4705fdb8a9a6656
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-12-04 19:34:21 +0900

    API 呼び出しを実装

--- App/a_as.s
+++ App/a_as.s
@@ -14,6 +14,10 @@
 .globl      api_linewin
 .globl      api_closewin
 .globl      api_getkey
+.globl      api_alloctimer
+.globl      api_inittimer
+.globl      api_settimer
+.globl      api_freetimer
 
 .text
 
@@ -183,4 +187,34 @@ api_getkey:
     MOVL    $15,    %EDX
     MOVL    4(%ESP),    %EAX    # mode
     INT     $0x40
+    RET
+
+api_alloctimer:
+    MOVL    $16,    %EDX
+    INT     $0x40
+
+api_inittimer:
+    PUSHL   %EBX
+    MOVL    $17,    %EDX
+    MOVL     8(%ESP),   %EBX
+    MOVL    12(%ESP),   %EAX
+    INT     $0x40
+    POPL    %EBX
+    RET
+
+api_settimer:
+    PUSHL   %EBX
+    MOVL    $18,    %EDX
+    MOVL     8(%ESP),   %EBX
+    MOVL    12(%ESP),   %EAX
+    INT     $0x40
+    POPL    %EBX
+    RET
+
+api_freetimer:
+    PUSHL   %EBX
+    MOVL    $19,    %EDX
+    MOVL    8(%ESP),    %EBX
+    INT     $0x40
+    POPL    %EBX
     RET
