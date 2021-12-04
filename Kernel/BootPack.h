3ce34b9b2f7797ea7a333d0f72da6d04dbfae321
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-12-04 18:14:04 +0900

    ウィンドウタイトルの色を変える関数を実装

--- Kernel/BootPack.h
+++ Kernel/BootPack.h
@@ -411,3 +411,4 @@ void putfonts8_asc_sht(struct SHEET *sht, int x, int y, int c, int b,
                        const char *s, int l);
 void make_textbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c);
 void make_wtitle8(unsigned char *buf, int xsize, char *title, char act);
+void change_wtitle8(struct SHEET *sht, char act);
