5d5de0a7e4ae1b2d2bd02ac88518cd6a101c243f
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-12-04 18:26:55 +0900

    アプリケーションが開いたシートの処理を修正

--- Kernel/Console.c
+++ Kernel/Console.c
@@ -335,7 +335,7 @@ int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline)
             shtctl = (struct SHTCTL *) *((int *) 0x0fe4);
             for (i = 0; i < MAX_SHEETS; ++ i) {
                 sht = &(shtctl->sheets0[i]);
-                if (sht->flags != 0 && sht->task == task) {
+                if ((sht->flags & 0x11) == 0x11 && sht->task == task) {
                     /*  アプリが開きっぱなしにした下じきを発見  */
                     sheet_free(sht);
                 }
@@ -375,6 +375,7 @@ int *hrb_api(int edi, int esi, int ebp, int esp,
     } else if (edx == 5) {
         sht = sheet_alloc(shtctl);
         sht->task = task;
+        sht->flags |= 0x10;
         sheet_setbuf(sht, (char *) ebx + ds_base, esi, edi, eax);
         make_window8((char *) ebx + ds_base, esi, edi,
                      (char *) ecx + ds_base, 0);
