e07de7fbc7bc23602a9b1e81cf835d3be4fff058
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-01-01 16:44:23 +0900

    リファクタリング

--- Kernel/BootPack.c
+++ Kernel/BootPack.c
@@ -480,23 +480,11 @@ void keywin_on(struct SHEET *key_win)
     return;
 }
 
-struct SHEET *open_console(struct SHTCTL *shtctl, unsigned int memtotal)
+struct TASK *open_constask(struct SHEET *sht, unsigned int memtotal)
 {
     struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
-
-    struct SHEET *sht = sheet_alloc(shtctl);
-    unsigned char *buf = (unsigned char *) memman_alloc_4k(
-            memman, CONSOLE_WIN_SIZE_X * CONSOLE_WIN_SIZE_Y);
     struct TASK *task = task_alloc();
     int *cons_fifo = (int *) memman_alloc_4k(memman, 128 * 4);
-
-    sheet_setbuf(sht, buf, CONSOLE_WIN_SIZE_X, CONSOLE_WIN_SIZE_Y, -1);
-    make_window8(buf, CONSOLE_WIN_SIZE_X, CONSOLE_WIN_SIZE_Y,
-                 "console", 0);
-    make_textbox8(sht, CURSOR_LEFT, CURSOR_TOP,
-                  (CONSOLE_COLS * CURSOR_WIDTH),
-                  (CONSOLE_ROWS * CURSOR_HEIGHT),
-                  COL8_000000);
 
     task->cons_stack = memman_alloc_4k(memman, 64 * 1024);
     task->tss.esp  = task->cons_stack + 64 * 1024 - 12;
@@ -511,9 +499,29 @@ struct SHEET *open_console(struct SHTCTL *shtctl, unsigned int memtotal)
     *((int *) (task->tss.esp + 8)) = memtotal;
     task_run(task, 2, 2);
 
-    sht->task = task;
-    sht->flags |= 0x20;     /*  カーソルあり。  */
     fifo32_init(&task->fifo, 128, cons_fifo, task);
+
+    return task;
+}
+
+struct SHEET *open_console(struct SHTCTL *shtctl, unsigned int memtotal)
+{
+    struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
+    struct SHEET *sht = sheet_alloc(shtctl);
+    unsigned char *buf = (unsigned char *) memman_alloc_4k(
+            memman, CONSOLE_WIN_SIZE_X * CONSOLE_WIN_SIZE_Y);
+
+    sheet_setbuf(sht, buf, CONSOLE_WIN_SIZE_X, CONSOLE_WIN_SIZE_Y, -1);
+    make_window8(buf, CONSOLE_WIN_SIZE_X, CONSOLE_WIN_SIZE_Y,
+                 "console", 0);
+    make_textbox8(sht, CURSOR_LEFT, CURSOR_TOP,
+                  (CONSOLE_COLS * CURSOR_WIDTH),
+                  (CONSOLE_ROWS * CURSOR_HEIGHT),
+                  COL8_000000);
+
+    sht->task = open_constask(sht, memtotal);
+    sht->flags |= 0x20;     /*  カーソルあり。  */
+
     return sht;
 }
 
--- Kernel/BootPack.h
+++ Kernel/BootPack.h
@@ -50,6 +50,7 @@ struct SHEET;
 #define KEYCMD_SENDTO_MOUSE     0xd4
 #define MOUSECMD_ENABLE         0xf4
 
+struct TASK *open_constask(struct SHEET *sht, unsigned int memtotal);
 struct SHEET *open_console(struct SHTCTL *shtctl, unsigned int memtotal);
 
 
