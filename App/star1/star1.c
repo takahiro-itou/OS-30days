d5c48b57331884138820fc9f8b84430b0b8c9841
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-01-16 15:11:02 +0900

    アプリ終了はキー入力を待ってからに変更

--- App/star1/star1.c
+++ App/star1/star1.c
@@ -11,5 +11,11 @@ void HariMain(void)
     win = api_openwin(buf, 150, 100, -1, "star1");
     api_boxfilwin(win,  6, 26, 143, 93, 0);
     api_point(win, 75, 59, 3);
+
+    for (;;) {
+        if (api_getkey(1) == 0x0a) {
+            break;      /*  Enter なら break;   */
+        }
+    }
     api_end();
 }
