2f3b44ff9ea0465b50aa9c3f1d00ef1f16363376
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-01-16 15:06:22 +0900

    アプリ終了はキー入力を待ってからに変更

--- App/winhelo/winhelo.c
+++ App/winhelo/winhelo.c
@@ -7,5 +7,11 @@ void HariMain(void)
 {
     int win;
     win = api_openwin(buf, 150, 50, -1, "hello");
+
+    for (;;) {
+        if (api_getkey(1) == 0x0a) {
+            break;      /*  Enter なら break;   */
+        }
+    }
     api_end();
 }
