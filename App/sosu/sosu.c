f30390d3c9f5101fe7c44d25ef7aea56f9c4bb5b
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-01-16 15:23:28 +0900

    アプリケーションを追加

---
+++ App/sosu/sosu.c
@@ -0,0 +1,26 @@
+
+#include "apilib.h"
+#include "stdio.h"
+
+#define MAX     1000
+
+void HariMain(void)
+{
+    char flag[MAX], s[8];
+    int i, j;
+
+    for (i = 0; i < MAX; ++ i) {
+        flag[i] = 0;
+    }
+    for (i = 2; i < MAX; ++ i) {
+        if (flag[i] == 0) {
+            /*  印がついていないので素数だ  */
+            snprintf(s, sizeof(s), "%d ", i);
+            api_putstr0(s);
+            for (j = i * 2; j < MAX; j += i) {
+                flag[j] = 1;    /*  倍数に印をつける。  */
+            }
+        }
+    }
+    api_end();
+}
