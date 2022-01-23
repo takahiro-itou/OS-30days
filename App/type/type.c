0218257c3c902875b92db4d276a1e206cbc45674
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2022-01-23 10:25:47 +0900

    アプリケーションを改良

--- App/type/type.c
+++ App/type/type.c
@@ -4,8 +4,13 @@
 void HariMain(void)
 {
     int fh;
-    char c;
-    fh = api_fopen("ipl.s");
+    char c, cmdline[30], *p;
+
+    api_cmdline(cmdline, 30);
+    for (p = cmdline; *p > ' '; ++ p) { }
+    for (; *p == ' '; ++ p) { }
+
+    fh = api_fopen(p);
     if (fh != 0) {
         for (;;) {
             if (api_fread(&c, 1, fh) == 0) {
@@ -13,6 +18,9 @@ void HariMain(void)
             }
             api_putchar(c);
         }
+    } else {
+        api_putstr0("File not found.\n");
     }
+
     api_end();
 }
